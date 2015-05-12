58ab69af72a2d880b0dbff12121a999fccbbc862
cardern <cardern@gmail.com>
2015-05-11 19:30:16 -0500

    Update - carousel, rentals, about

--- js/application.js
+++ js/application.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
  
   $("#intro-carousel").owlCarousel({
  
-      navigation : false, // Show next and prev buttons
-      autoPlay:true,
+      navigation:false, // Hide next and prev buttons
+      autoplay:true,
+      autoplaySpeed:3000,
       mouseDrag:false,
       touchDrag:false,
-      singleItem:true
- 
+      autoWidth:true,
+      loop:true
+
       // "singleItem:true" is a shortcut for:
       // items : 1, 
       // itemsDesktop : false,
